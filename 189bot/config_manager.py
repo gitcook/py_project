4a78958692f55d16a793629374353aee9854dca2
gitcook <19466154+gitcook@users.noreply.github.com>
2025-12-30 21:42:13 +0800

    189bot bug

--- 189bot/config_manager.py
+++ 189bot/config_manager.py
@@ -37,7 +37,7 @@ def load_config():
             "DB_RETENTION_DAYS": 30
         },
         "DRIVE_SWITCHES": {"ENABLE_189": True, "ENABLE_UC": False, "ENABLE_123": False},
-        "FILTERING": {"EXCLUDE_KEYWORDS": ["小程序", "预告", "书籍", "电子书", "课程", "教程", "写真"]}, 
+        "FILTERING": {"EXCLUDE_KEYWORDS": ['小程序', '预告', '预感', '盈利', '即可观看', '书籍', '电子书', '图书', '丛书', '期刊','app','软件', '破解版','解锁','专业版','高级版','最新版','食谱', '免安装', '免广告','安卓', 'Android', '课程', '作品', '教程', '教学', '全书', '名著', 'mobi', 'MOBI', 'epub','任天堂','PC','单机游戏', 'pdf', 'PDF', 'PPT', '抽奖', '完整版', '有声书','读者','文学', '写作', '节课', '套装', '话术', '纯净版', '日历', 'txt', 'MP3','网赚', 'mp3', 'WAV', 'CD', '音乐', '专辑', '模板', '书中', '读物', '入门', '零基础', '常识', '电商', '小红书','JPG','短视频','工作总结', '哈哈哈哈哈', '写真','抖音', '资料', '华为', '短剧', '纪录片', '记录片', '纪录', '纪实', '学习', '付费', '小学', '初中','数学', '语文', '唐诗','魔法坏女巫','车载','DJ','合并', '演唱会', '综艺']}, 
         "RULES": {
             "API_CONFIGS": [
                 {
@@ -45,7 +45,7 @@ def load_config():
                     'folder_prefix': "剧集/",
                     'priority_keywords': ["权力的游戏", "绝命毒师"], 
                     'required_keywords': [], 
-                    'optional_keywords': ["季", "集", "EP", "S0", "美剧", "英剧"], 
+                    'optional_keywords': ["季", "集", "EP", "S0", "美剧", "英剧", "日剧", "韩剧"], 
                     'excluded_keywords': ["P5", "Profile 5", "DVP5"], 
                     'try_join': True
                 },
@@ -54,7 +54,7 @@ def load_config():
                     'folder_prefix': "电影/",
                     'priority_keywords': [],
                     'required_keywords': [], 
-                    'optional_keywords': ["原盘", "REMUX", "4K", "HDR","简中" ],
+                    'optional_keywords': ["原盘", "REMUX", "4K", "HDR","简中", "简英", "简繁", "双语", "HD", "60 FPS" ],
                     'excluded_keywords': ["枪版", "抢先版", "TS", "TC", "1080", "1080p", "S0", "EP", "更至", "完结"],
                     'try_join': True
                 }
